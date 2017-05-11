11b31dec1d2fb21f336b6178a72c7a2a3a718611
Marco Speronello <Marco Speronello>
2017-05-11 14:10:24 +0200

    Pulizia codice, refactoring, aggiunta commenti

--- src/main/java/it/unipd/dei/dm1617/release/DMProject.java
+++ src/main/java/it/unipd/dei/dm1617/release/DMProject.java
@@ -20,25 +20,27 @@ import org.apache.log4j.Level;
 
 
 /**
- * Example program to show the basic usage of some Spark utilities.
+ * Main class managing the input dataset and the k-median clustering.
+ * Reads the input data, lemmatizing it and representing it in TF-IDF format and runs the
+ * clustering algorithm.
  */
 public class DMProject {
 
   public static void main(String[] args) {
-    // Removes all the infodump shown on terminal by spark
+    // Removes all the infoormation shown on terminal by Spark.
     Logger.getLogger("org").setLevel(Level.OFF);
     Logger.getLogger("akka").setLevel(Level.OFF);
 
     String dataPath = args[0];
 
-    // Usual setup
+    // Initializes Spark context.
     SparkConf conf = new SparkConf(true).setAppName("DMProject");
     JavaSparkContext sc = new JavaSparkContext(conf);
 
     
     // ---------- UNCOMMENT THIS ON FIRST RUN --------------- //
     // Lemmatize pages and saves them to a file so that we dont have to recompute the
-    // lematization everytime
+    // lematization every time.
     // 
     // //Load dataset of pages
     // JavaRDD<WikiPage> pages = InputOutput.read(sc, dataPath);
@@ -46,7 +48,7 @@ public class DMProject {
     // JavaRDD<WikiPage> lemmatizedPages = Lemmatizer.lemmatizeWikiPages(pages).cache();
     // InputOutput.write(lemmatizedPages, "small-dataset-lemmas");
     
-    // Reads lemmatized wikipedia pages
+    // Reads lemmatized wikipedia pages.
     JavaRDD<WikiPage> lemmatizedPages = InputOutput.read(sc, "small-dataset-lemmas").cache();
 
     JavaRDD<ArrayList<String>> lemmas = lemmatizedPages.map((p) -> {
@@ -54,68 +56,32 @@ public class DMProject {
     }).cache();
 
 
-    // Transform the sequence of lemmas in vectors of counts in a
-    // space of 100 dimensions, using the 100 top lemmas as the vocabulary.
-    // This invocation follows a common pattern used in Spark components:
-    //
-    //  - Build an instance of a configurable object, in this case CountVectorizer.
-    //  - Set the parameters of the algorithm implemented by the object
-    //  - Invoke the `transform` method on the configured object, yielding
-    //  - the transformed dataset.
-    //
-    // In this case we also cache the dataset because the next step,
-    // IDF, will perform two passes over it.
+    // Transform the sequence of lemmas in vectors of counts in a space of the specified number of
+    // dimensions, using the said number of top lemmas as the vocabulary.
     JavaRDD<Vector> tf = new CountVectorizer()
       .setVocabularySize(300)
       .transform(lemmas)
       .cache();
 
-    // Same as above, here we follow the same pattern, with a small
-    // addition. Some of these "configurable" objects configure their
-    // internal state by means of an invocation of their `fit` method
-    // on a dataset. In this case, the Inverse Document Frequence
-    // algorithm needs to know about the term frequencies across the
-    // entire input dataset before rescaling the counts of the single
-    // vectors, and this is what happens inside the `fit` method invocation.
+    // Converts the data in a TF-IDF representation.
     JavaRDD<Vector> tfidf = new IDF()
       .fit(tf)
       .transform(tf);
 
-
-
+    // Sets the number of cluster and maximum iterations.
     int[] numClusters = {9000};
     int maxIterations = 1000;
 
-    System.out.println("Total points:" + tfidf.count());
+    System.out.println("Total points:" + tfidf.count() +"\n\n");
     
-    for(int k : numClusters)
-    {
+    // Computes k-median clustering of the TF-IDF dataset.
+    for(int k : numClusters) {
         long start = System.nanoTime();
-        System.out.println("");
-        System.out.println("");
         KMedoids kmed = new KMedoids(k, maxIterations, 1000L);
         kmed.run(tfidf, sc);
         double finish = (System.nanoTime() - start) / 1e9;
         System.out.println("Done for k="+k+" in "+finish);
     }
-
-    
-    for(int k : numClusters)
-    {    
-        long start = System.nanoTime();
-
-        KMeansModel clusters = KMeans.train(tfidf.rdd(), k, maxIterations, "random", 1000L);
-        
-        
-        double cost = clusters.computeCost(tfidf.rdd());
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Cost: " + cost);
-
-        double finish = (System.nanoTime() - start) / 1e9;
-        System.out.println("Done for k="+k+" in "+finish);   
-    }
-
 
   }
 
--- src/main/java/it/unipd/dei/dm1617/release/KMedoids.java
+++ src/main/java/it/unipd/dei/dm1617/release/KMedoids.java
@@ -13,7 +13,6 @@ import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.util.random.XORShiftRandom;
 import org.apache.spark.util.DoubleAccumulator;
 import scala.Tuple2;
-import scala.Tuple3;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -21,280 +20,148 @@ import java.util.List;
 import java.util.Random;
 import java.util.Map;
 import java.io.Serializable;
+import java.lang.Math;
 
+/**
+ * Class implementing all the necessary methods to perform the k-medoids clustering using cosine
+ * distance.
+ */
 public class KMedoids implements Serializable{
 	private double epsilon = 1e-4;
 	private int maxIterations;
 	private int k;
 	private long seed;
-
-	public KMedoids(int k, int maxIterations, long seed){
+ 
+	public KMedoids(int k, int maxIterations, long seed) {
 		this.k = k;
 		this.maxIterations = maxIterations;
-		// this.seed = new Random().nextLong();
 		this.seed = seed;
 	}
 
-	public void run(JavaRDD<Vector> data, JavaSparkContext sc)
-	{
+	// Prepares the input data and runs the clustering algorithm.
+	public void run(JavaRDD<Vector> data, JavaSparkContext sc) {
+		// Computes and caches the norms of the input vectors.
 		JavaRDD<Double> norms = data.map((v) -> Vectors.norm(v, 2.0));
 		norms.cache();
+		// Maps the vectors with norm to a JavaRDD.
 		JavaRDD<VectorWithNorm> zippedData = data.zip(norms).map((tuple) -> new VectorWithNorm(tuple._1,tuple._2));
-		
-		runAlgorithmMedoids(zippedData,sc);
+		// Runs the clustering algorithm.
+		kMedoids(zippedData,sc);
 	}
 
-	// private void runAlgorithm(JavaRDD<VectorWithNorm> data, JavaSparkContext sc){
 
-	// 	List<VectorWithNorm> centers = new ArrayList<>(initRandom(data));
-
-	// 	boolean converged = false;
-	// 	double cost = 0.0;
-	// 	int iteration = 0;
-
-	// 	while(iteration < maxIterations && !converged)
-	// 	{
-	// 		DoubleAccumulator costAccum = sc.sc().doubleAccumulator();
-	// 		Broadcast<List<VectorWithNorm>> bcCenters = sc.broadcast(centers);
-
-	// 		Map<Integer,Tuple2<Vector,Long>> totalContribs = data.mapPartitionsToPair((points) -> {
-	// 			List<VectorWithNorm> thisCenters = bcCenters.value();
-	// 			int dims = thisCenters.get(0).vector.size();
-
-	// 			// inizializza (tutti i componenti di tutti i vettori di) sums a zero,
-	// 			// e tutti i componenti di counts a zero. Sums e counts hanno dimensione
-	// 			// pari al numero di centri
-	// 			List<Vector> sums = new ArrayList<Vector>();
-	// 			long[] counts = new long[thisCenters.size()];
-	// 			for(int i=0; i<thisCenters.size(); i++)
-	// 				sums.add(Vectors.zeros(dims));
-	// 			Arrays.fill(counts, 0L);
-
-	// 			while(points.hasNext()){
-	// 				VectorWithNorm point = points.next();
-	// 				// ritorna indice del centro più vicino a point e la sua distanza da esso
-	// 				Tuple2<Integer, Double> bestPartition = findClosest(thisCenters, point);
-	// 				int bestCenter = bestPartition._1;
-	// 				double pointCost = bestPartition._2;
-	// 				// somma la distanza tra punto e centro più vicino alla costo della funzione obiettivo
-	// 				costAccum.add(pointCost);
-	// 				// prende il vettore centro di indice corrispondente a bestCenter
-	// 				Vector sum = sums.get(bestCenter);
-	// 				// somma point a sum (sum = sum + point) (componentwise)
-	// 				// la componente i-esima di sum contiene la somma delle componenti i-esime dei vettori assegnati a quel centro		
-	// 				BLAS.axpy(1.0, point.vector, sum);
-	// 				counts[bestCenter]++;
-	// 			}
-
-	// 			List<Tuple2<Integer, Tuple2<Vector, Long>>> partitionCounts = new ArrayList<>();
-
-	// 			// per ogni centro
-	// 			for(int i=0; i < counts.length; i++)
-	// 			{	
-	// 				// se quel centro ha almeno un punto assegnato ad esso, aggiunge a partitionCounts
-	// 				// una cosa del tipo <indice del centro, <costo del cluster corrispondente, numero di punti del cluster>>
-	// 				if(counts[i]>0)
-	// 					partitionCounts.add(new Tuple2<>(i, new Tuple2<>(sums.get(i), counts[i])));
-	// 			}
-
-	// 			return partitionCounts.iterator();
-	// 		}).reduceByKey((tuple1, tuple2) -> {
-
-	// 			Vector sum1 = tuple1._1;
-	// 			Vector sum2 = tuple2._1;
-	// 			long count1 = tuple1._2;
-	// 			long count2 = tuple2._2;
-				
-	// 			// somma sum2 a sum1 (sum1 = sum1 + sum2) (componentwise)
-	// 			// sta effettuando la somma dei vettori dei punti di un cluster				
-	// 			BLAS.axpy(1.0, sum2, sum1);
-
-	// 			return new Tuple2<>(sum1, count1 + count2);
-	// 		}).collectAsMap();
-
-	// 		bcCenters.destroy();
-
-	// 		converged = true;
-	// 		for(Map.Entry<Integer, Tuple2<Vector,Long>> entry : totalContribs.entrySet())
-	// 		{
-	// 			int i = entry.getKey();
-	// 			Vector sum = entry.getValue()._1;
-	// 			double count = entry.getValue()._2;
-				
-	// 			//sum = (1.0/count) * sum = sum/count (componentwise)
-	// 			BLAS.scal(1.0 / count, sum);
-	// 			//nuovo centroide
-	// 			VectorWithNorm newCenter = new VectorWithNorm(sum);
-	// 			if (converged && fastSquaredDistance(newCenter, centers.get(i)) > epsilon*epsilon)
-	// 				converged = false;
-	// 			centers.set(i, newCenter);
-	// 		}
-	// 		cost = costAccum.value();
-	// 		iteration++;
-	// 	}
-	// 	System.out.println("KMeans converged in "+iteration+" iterations with cost "+cost);
-	// }
-
-	private void runAlgorithmMedoids(JavaRDD<VectorWithNorm> data, JavaSparkContext sc){
- 
+	// Performs the k-medoids clustering of the input dataset.
+	private void kMedoids(JavaRDD<VectorWithNorm> data, JavaSparkContext sc) {
+ 		// Chooses a list of k random centers.
         List<VectorWithNorm> centers = new ArrayList<>(initRandom(data));
- 
+ 		
         boolean converged = false;
         double cost = 0.0;
         int iteration = 0;
- 
+ 		
+ 		// Iterates until the maximum number of iterations has been reached or the objective
+ 		// function converges.
         while(iteration < maxIterations && !converged)
         {
             System.out.println("Inizio iterazione " + iteration);
+            // Objective function cost accumulator.
             DoubleAccumulator costAccum = sc.sc().doubleAccumulator();
+            // Broadcasts the center list to all the workers.
             Broadcast<List<VectorWithNorm>> bcCenters = sc.broadcast(centers);
- 
-            Map<Integer, Tuple2<List<VectorWithNorm>, Double>> totalContribs = data.mapPartitionsToPair((points) -> {
+ 			
+ 			// Partitions the points into clusters. The key field of the map represents the index
+ 			// of the cluster, while the value field contains the points of the cluster.
+            Map<Integer, List<VectorWithNorm>> totalContribs = data.mapPartitionsToPair((points) -> {
                 List<VectorWithNorm> thisCenters = bcCenters.value();
-                int dims = thisCenters.get(0).vector.size(); // dimension of a vector representing a document (equal to "vocabSize")
- 
+                // Number of elements of a vector representing a document (equal to "vocabSize").
+                int dims = thisCenters.get(0).vector.size(); 
+ 				
+ 				// Initializes the list of clusters.
                 List<List<VectorWithNorm>> thisPartition = new ArrayList<List<VectorWithNorm>>(k);
                 for(int i=0; i<k; i++)
                     thisPartition.add(new ArrayList<VectorWithNorm>());
-                double[] thisCost = new double[k];
-                Arrays.fill(thisCost, 0.0);
- 
+ 				
+ 				// Adds each input point to its respective cluster, ignoring points with all fields
+ 				// set to zero.
                 while(points.hasNext()){
                     VectorWithNorm point = points.next();
                     if(point.vector.numNonzeros()!=0){
-	                    Tuple2<Integer, Double> bestPartition = findClosestCosine(thisCenters, point);
-	                    // bestPartition._1 contains the index of the cluster to which this point as been assigned
-	                    // bestPartition._2 indicates the cost (i.e. the distance) between the center of the cluster and this point
-	                    costAccum.add(bestPartition._2);
-	                  
+                    	// Finds the closest center to the input point and their distance.
+	                    Tuple2<Integer, Double> bestPartition = findClosestCosine(thisCenters, point);	                    
+
+	                    // Increases the objective function cost by the squared distance between
+	                    // the point and its cluster's center.
+	                    costAccum.add(Math.pow(bestPartition._2,2));
+	                    
+	                    // Adds the point to its cluster.
 	                    thisPartition.get(bestPartition._1).add(point);
-	                    thisCost[bestPartition._1] += bestPartition._2;
 	                }
                 }
+ 				
+                List<Tuple2<Integer, List<VectorWithNorm>>> partitions = new ArrayList<>();
  
-                List<Tuple2<Integer, Tuple2<List<VectorWithNorm>, Double>>> partitions = new ArrayList<>();
- 
-                // for each cluster, initialize a tuple2 containing the index of the cluster,
-                // the list of the points belonging to that cluster, and the total cost of the points for that cluster
-                for(int i=0; i<k; i++)
-                {
+                // For each cluster, initializes a tuple containing the index of the cluster and
+                // the list of the points belonging to that cluster.
+                for(int i=0; i<k; i++) {
                     if(thisPartition.get(i).size()>0)
-                        partitions.add(new Tuple2<>(i, new Tuple2<>(thisPartition.get(i), thisCost[i])));
+                        partitions.add(new Tuple2<>(i, thisPartition.get(i)));
                 }
  
                 return partitions.iterator();
             }).reduceByKey((tuple1, tuple2) -> {
-                // merges all the points belonging to one cluster assigned by the different workers
+                // Merges all the points belonging to one cluster assigned by the different workers.
                 List<VectorWithNorm> clusterPoints = new ArrayList<VectorWithNorm>();
-                clusterPoints.addAll(tuple1._1);
-                clusterPoints.addAll(tuple2._1);
+                clusterPoints.addAll(tuple1);
+                clusterPoints.addAll(tuple2);
  
-                double clusterCost = tuple1._2 + tuple2._2;
-                return new Tuple2<>(clusterPoints, clusterCost);
+                return clusterPoints;
             }).collectAsMap();
             System.out.println("Partizionamento completato iterazione " + iteration);
+
+            // Removes the broadcast variable.
             bcCenters.destroy();
-
-            // int numPoints = 0;
-            // for(Map.Entry<Integer, Tuple2<List<VectorWithNorm>, Double>> entry : totalContribs.entrySet()) {
-            // 	System.out.println("Cost for cluster "+entry.getKey()+": "+entry.getValue()._2+"; Number of points: "+entry.getValue()._1.size());
-            // 	numPoints+=entry.getValue()._1.size();
-            // }
-
- 
-            // Adesso totalContribs contiene tutti i punti appartenenti ad un dato cluster assieme al costo di quel cluster
-            // all'interno di una mappa. La struttura è qualcosa di questo tipo:
-            // ---------------------------------------------------
-            // | ClusterIndex | Points                    | Cost |
-            // ---------------------------------------------------
-            // |            0 | ArrayList<VectorWithNorm> | 15.3 |
-            // |            1 | ArrayList<VectorWithNorm> | 152.8|
-            // ---------------------------------------------------
-            //
-            // Quindi per andare a calcolare il nuovo medoide servirà fare qualcosa di questo tipo;
-            //
-            //
-            //
-            // Foreach(entry in totalContribs)
-            //   int i = entry.getKey()
-            //   List<VectorWithNorm> points = entry.getValue()._1
-            //   double clusterCost = entry.getValue()._2
-            //   double minCost = Double.POSITIVE_INFINITY;
-            //   if(clusterCost!=0.0) // Calcola solo se ci sono punti nel cluster
-            //      foreach(point in points) // per ogni punto del cluster i
-            //          double newCost = calculateClusterCost(points, point) // calcola la funzione costo utilizzando come "point" come medoide
-            //          if(newCost<minCost)
-            //              minCost = newCost;
-            //              centers.set(i, point) // imposta come nuovo medoide il punto corrente
-            //
-            // converged = true
-            // for(i=0 to k)
-            //   * controlla che la nuova distanza dei nuovi medoidi dai vecchi sia > epsilon*epsilon *
-            //   * oppure si può semplicemente controllare se il vecchio costo sia migliore di un certo epsilon rispetto al vecchio *
  			
  			System.out.println("Inizio calcolo medoidi iterazione " + iteration);
  			long start = System.nanoTime();
             List<Tuple2<Integer, VectorWithNorm>> medoids = new ArrayList<Tuple2<Integer, VectorWithNorm>>();
- 
-            for(Map.Entry<Integer, Tuple2<List<VectorWithNorm>, Double>> entry : totalContribs.entrySet()) {
+ 			
+ 			// Computes the medoid for each cluster. Each medoid becomes the new center for its
+ 			// cluster.
+            for(Map.Entry<Integer, List<VectorWithNorm>> entry : totalContribs.entrySet()) {
                 int i = entry.getKey();
-                List<VectorWithNorm> points = entry.getValue()._1;
-                medoids.add(new Tuple2<>(i,computeMedoid(points)));
+                List<VectorWithNorm> points = entry.getValue();
+                VectorWithNorm newMedoid = computeMedoid(points);
+                medoids.add(new Tuple2<>(i, newMedoid));
+                centers.set(i, newMedoid);
             }
             double finish = (System.nanoTime() - start) / 1e9;
-            System.out.println("Calcolo medoidi completato iterazione " + iteration + " in "+finish);
- 
-            converged = true;
-            if (converged && Math.abs(cost-costAccum.value()) > epsilon*epsilon)
-                converged = false;
-            for(Tuple2<Integer, VectorWithNorm> newMedoid : medoids) {
-                centers.set(newMedoid._1(), newMedoid._2()); // sta roba si può fare direttamente dentro al for precedente
-            }
- 
+            System.out.println("Calcolo medoidi completato iterazione " + iteration + " in " + finish);
+ 			
+ 			// If the difference between the current cost (costAccum) and the cost computed during
+ 			// the previous iteration is not bigger than the specified threshold, then the
+ 			// objective function still has not converged.
+            if (Math.abs(cost-costAccum.value()) < epsilon*epsilon)
+                converged = true;
+
+ 			// Updates the cost.
             cost = costAccum.value();
             System.out.println("Fine iterazione " + iteration + "; costo:" + cost);
             iteration++;
         }
-        System.out.println("KMedoids converged in "+iteration+" iterations with cost "+cost);
+        System.out.println("KMedoids converged in " + iteration + " iterations with cost " + cost);
     }
 
-	/*private List<VectorWithNorm> initRandom(JavaRDD<VectorWithNorm> data)
-	{
-		return data.takeSample(false, k, new XORShiftRandom(seed).nextInt());	
-	}*/
-
-	private List<VectorWithNorm> initRandom(JavaRDD<VectorWithNorm> data)
-	{
+    // Randomly chooses k points from the input data as cluster centers.
+	private List<VectorWithNorm> initRandom(JavaRDD<VectorWithNorm> data) {
 		List<VectorWithNorm> list = data.takeSample(false, k, new XORShiftRandom(seed).nextInt());
 		for (int i=0; i<list.size(); i++)
 			list.get(i).setCenterIndex(i);
 		return list;
 	}
 
-	private Tuple2<Integer,Double> findClosest(List<VectorWithNorm> centers, VectorWithNorm point)
-	{
-		double bestDistance = Double.POSITIVE_INFINITY;
-		int bestIndex = 0;
-		int i = 0;
-		for(VectorWithNorm center : centers)
-		{
-			double lowerBoundOfSqDistance = center.norm - point.norm;
-			lowerBoundOfSqDistance = lowerBoundOfSqDistance * lowerBoundOfSqDistance;
-			if(lowerBoundOfSqDistance < bestDistance)
-			{
-				double distance = fastSquaredDistance(center, point);
-				if(distance < bestDistance)
-				{
-					bestDistance = distance;
-					bestIndex = i;
-				}
-			}
-			i++;
-		}
-		return new Tuple2<>(bestIndex, bestDistance);
-	}
-
-	private VectorWithNorm computeMedoid(List<VectorWithNorm> vectors){
+	// Computes the medoid (the point that minimizes the sum of distances to the other points of
+	// the cluster) of the input vectors.
+	private VectorWithNorm computeMedoid(List<VectorWithNorm> vectors) {
 		double bestDistance = Double.POSITIVE_INFINITY;
 		int bestIndex = 0;
 		for(int i=0; i<vectors.size(); i++) {
@@ -302,7 +169,7 @@ public class KMedoids implements Serializable{
 			for(int j=0; j<vectors.size(); j++)
 				if (i!=j)
 					tempDistance += cosineDistance(vectors.get(i),vectors.get(j));
-			if(tempDistance < bestDistance){
+			if(tempDistance < bestDistance) {
 				bestDistance = tempDistance;
 				bestIndex = i;
 			}
@@ -310,20 +177,14 @@ public class KMedoids implements Serializable{
 		return vectors.get(bestIndex);
 	}
 
-	private double fastSquaredDistance(VectorWithNorm v1, VectorWithNorm v2){
-		return Vectors.sqdist(v1.vector, v2.vector);
-	}
-
-	private Tuple2<Integer, Double> findClosestCosine(List<VectorWithNorm> centers, VectorWithNorm point)
-	{
+	// Finds the closest center to the input vector using cosine distance.
+	private Tuple2<Integer, Double> findClosestCosine(List<VectorWithNorm> centers, VectorWithNorm point) {
 		double bestDistance = Double.POSITIVE_INFINITY;
 		int bestIndex = 0;
 		int i = 0;
-		for(VectorWithNorm center : centers)
-		{
+		for(VectorWithNorm center : centers) {
 			double distance = cosineDistance(center, point);
-			if(distance < bestDistance)
-			{
+			if(distance < bestDistance) {
 				bestDistance = distance;
 				bestIndex = i;
 			}
@@ -333,17 +194,11 @@ public class KMedoids implements Serializable{
 		return new Tuple2<>(bestIndex, bestDistance);
 	}
 
+	// Returns the cosine distance between the input vectors.
 	private static double cosineDistance(VectorWithNorm v1, VectorWithNorm v2) {
 	    double cosine = BLAS.dot(v1.vector, v2.vector) / (v1.norm * v2.norm);
-	    if (cosine > 1.0) {
-	        // Mathematically, this should't be possible, but due to the
-	        // propagation of errors in floating point operations, it
-	        // happens
+	    if (cosine > 1.0)
 	        cosine = 1;
-	    }
-	    // If you wish to use this function with vectors that can have
-	    // negative components (like the ones given by word2vec), then
-	    // rescale by PI instead of PI/2
 	    return (2 / Math.PI) * Math.acos(cosine);
-	  }
+	 }
 }
